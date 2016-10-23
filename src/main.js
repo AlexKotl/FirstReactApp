a8620a3193ea18064cccac0d284ea6922e78dab9
Alexander Kotliarov <slicer256@gmail.com>
2016-10-23 22:01:25 +0300

    display news list using props

--- src/main.js
+++ src/main.js
@@ -1,6 +1,48 @@
-var element = document.getElementById('root');
+var my_news = [
+    {
+        author: 'Alex',
+        text: 'Text news...'
+    },
+    {
+        author: 'Jack',
+        text: 'Another test'
+    },
+    {
+        author: 'Bob',
+        text: 'Test here.'
+    }
+];
+
+var News = React.createClass({
+    render: function() {
+        const news_html = this.props.data.map(function(item, index) {
+            return (
+                <div key={index}>
+                    <b className="news__author">{item.author}</b>
+                    {item.text}
+                </div>
+            )
+        })
+
+        return (
+            <div className="news">
+                {news_html}
+            </div>
+        )
+    }
+})
+
+var App = React.createClass({
+    render: function() {
+        return (
+            <div className="app">
+                <News data={my_news} /> {/* some comment */}
+            </div>
+        )
+    }
+});
 
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    element
+    <App />,
+    document.getElementById('root')
 );
