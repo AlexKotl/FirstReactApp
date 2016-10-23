a4b63cb60fda2ab41a670c5d8e2754049a797656
Alexander Kotliarov <slicer256@gmail.com>
2016-10-24 00:25:46 +0300

    added button to expand item text

--- src/main.js
+++ src/main.js
@@ -21,6 +21,17 @@ var Article = React.createClass({
         })
     },
 
+    getInitialState: function() {
+        return {
+            visible: false
+        }
+    },
+
+    detailsClick: function(event) {
+        event.preventDefault();
+        this.setState({visible: true});
+    },
+
     render: function() {
         const data = this.props.data;
 
@@ -28,6 +39,8 @@ var Article = React.createClass({
             <div className="news__article">
                 <b className="news__author">{data.author}</b> <br/>
                 {data.text}
+                <div className={this.state.visible ? '' : 'hidden'}>Full article comming soon...</div>
+                <p><a href="#" className={'news__details ' + (!this.state.visible ? '' : 'hidden')} onClick={this.detailsClick}>More details...</a> </p>
             </div>
         )
     }
